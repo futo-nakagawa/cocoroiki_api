97e2c3e127b532ca51bb93711be442fb4f3aed7b
Kid キッド <2200339@ecc.ac.jp>
2023-05-30 11:45:54 +0900

    `add` main code

--- main.py
+++ main.py
@@ -1,11 +1,81 @@
-from fastapi import FastAPI
+import json
+import pathlib
+from typing import List, Union
+from fastapi import FastAPI, Response
+from models import Track
 
 app = FastAPI()
+data = []
+@app.on_event("startup")
+async def startup_event():
+    datapath = pathlib.Path() / "data" / "tracks.json"
+    with open(datapath, "r") as f:
+        tracks = json.load(f)
+        for track in tracks:
+            data.append(Track(**track).dict())
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# GET all the tracks
+@app.get("/tracks/", response_model=List[Track])
+def track():
+    return data
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+# GET by id
+@app.get("/tracks/{track_id}", response_model=Union[Track, str])
+def track(track_id: int, response: Response):
+    #find the track with the given ID, or None if it does not exist
+    track = None
+    for t in data:
+        if t["id"] == track_id:
+            track = t
+            break
+
+    if track is None:
+        response.status_code = 404
+        return "Not found"
+    return track
+
+#POST
+@app.post("/tracks/", response_model=Track, status_code=201)
+def create_track(track: Track):
+    track_dict = track.dict()
+    track_dict["id"] = max(data, key=lambda x:x ["id"]).get("id") + 1
+    data.append(track_dict)
+    return track_dict
+
+#PUT
+@app.put("/tracks/{track_id}", response_model=Union[Track, str])
+def track(track_id: int, updated_track: Track, response: Response):
+    #find the track with the given ID, or None if it does not exist
+    track = None
+    for t in data:
+        if t["id"] == track_id:
+            track = t
+            break
+
+    if track is None:
+        response.status_code = 404
+        return "Not found"
+    
+    for key, val in updated_track.dict().items():
+        if key != "id":
+            track[key] = val
+    return track
+
+#DELETE by id
+@app.delete("/tracks/{track_id}/")
+def track(track_id: int, response: Response):
+    track_index = None
+    # get the index of the track to delete
+    for idx ,t in enumerate(data):
+        if t["id"] == track_id:
+            track_index = idx
+            break
+
+    if track_index is None:
+        # if a track with given ID doesn't exist, set 404 code and return string
+        response.status_code = 404
+        return "Not found"
+    
+    # delete the track from the data, and return empty 200 response
+    del data[track_index]
+    return Response(status_code=200)
