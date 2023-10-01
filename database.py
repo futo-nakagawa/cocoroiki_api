9fcedf8c6ff752f283235c7826fe7fa9246b6d70
yumekiti <yumekiti120440@gmail.com>
2023-10-02 02:22:31 +0900

    chore: add dockerfile

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
  
 # Create a sqlite engine instance
-engine = create_engine("mysql://cocoroiki:password@db/cocoroiki?charset=utf8mb4")
+engine = create_engine("mysql://cocoroiki:password@cocoroiki-db-service/cocoroiki?charset=utf8mb4")
 
 # Create a DeclarativeMeta instance
 Base = declarative_base()
